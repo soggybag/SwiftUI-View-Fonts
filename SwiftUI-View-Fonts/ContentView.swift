b1704b7987d9b565ce84ce008be69917e6a3d2e7
Mitchell Hudson <soggybag@gmail.com>
2025-06-11 08:06:03 -0700

    Initial Commit

---
+++ SwiftUI-View-Fonts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-View-Fonts
+//
+//  Created by mitchell hudson on 6/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-View-Fonts/SwiftUI_View_FontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_View_FontsApp.swift
+//  SwiftUI-View-Fonts
+//
+//  Created by mitchell hudson on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_View_FontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
